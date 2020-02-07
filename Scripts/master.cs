fbe9d89e20d34ea76d93442a4fecef0651f8b033
Jenetrix <>
2020-02-07 11:14:47 -0700

    Add most recent master.cs (Last Updated 12/09/18)

--- Scripts/master.cs
+++ Scripts/master.cs
@@ -1,7 +1,16 @@
+//---------------------------------
+// master.cs
+// Last Updated 12/09/18
+//---------------------------------
 
-$Inet::Master1 = "IP:ss1m1.masters.dynamix.com:29000";
-$Inet::Master2 = "IP:ss1m2.masters.dynamix.com:29000";
-$Inet::Master3 = "IP:ss1m3.masters.dynamix.com:29000";
+$Inet::Master1 = "IP:master1.starsiege.io:29000";    // Jenetrix's Master
+$Inet::Master2 = "IP:master2.starsiege.pw:29000";    // Wilzuun's Master
+$Inet::Master3 = "IP:master3.starsiege.io:29000"; 
+
+// Legacy Master Servers
+// $Inet::Master1 = "IP:ss1m1.masters.dynamix.com:29000";
+// $Inet::Master2 = "IP:ss1m2.masters.dynamix.com:29000";
+// $Inet::Master3 = "IP:ss1m3.masters.dynamix.com:29000";
 
 $inet::IPBroadcast1 = "IP:broadcast:29001";
 $inet::IPXBroadcast1 = "IPX:broadcast:29001";
